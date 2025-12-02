afb69fad876f80a5563c485403ab5e11d8bacb7a
mayffi <mayfred.appiah@gmail.com>
2025-12-02 22:33:51 +0200

    fix workflow

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   // For local development, use '/'
