61cc801fa5ebcc68fd2156935ab05546281627fb
mayffi <mayfred.appiah@gmail.com>
2025-12-02 22:53:13 +0200

    fix path

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // For local development, use '/'
-  // For GitHub Pages, change to '/Wedding_web/' before building
-  base: '/',
+  // For GitHub Pages, change to '/wedding-website/' before building
+  base: '/wedding-website/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
